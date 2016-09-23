22b5a4621684ae92603c4dd16f32bc190e4009f5
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-23 19:53:19 +0000

    new file: ps_config/bin/get_config_section_list.py

---
+++ ps_config/bin/get_config_section_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import argparse
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+from ps_config import lib
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="config ファイルから section の一覧を取得します。")
+    parser.add_argument(
+        "path",
+        help="config ファイルのパス。")
+
+    args = parser.parse_args()
+
+    config=lib.ps_config(args.path)
+    result=config.get_section_list()
+
+    if len(result)>0:
+        for i in result:
+            print(i)
+
+if __name__ == "__main__":
+    main()
