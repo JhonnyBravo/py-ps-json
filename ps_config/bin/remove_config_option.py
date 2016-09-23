7f0993eee58868f489ca88a45495dfb0c330b322
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-23 19:53:53 +0000

    new file: ps_config/bin/remove_config_option.py

---
+++ ps_config/bin/remove_config_option.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import argparse
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+from ps_config import lib
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="config ファイルから option を削除します。")
+    parser.add_argument(
+        "path",
+        help="config ファイルのパス。")
+    parser.add_argument(
+        "section_name",
+        help="section の名前。")
+    parser.add_argument(
+        "option_name",
+        help="削除する option の名前。")
+
+    args = parser.parse_args()
+
+    config=lib.ps_config(args.path)
+    config.remove_option(args.section_name,args.option_name)
+
+if __name__ == "__main__":
+    main()
