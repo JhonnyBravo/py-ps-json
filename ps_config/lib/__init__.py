89eec62529aec1622b642dae5c1481bbe60b94be
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-23 19:46:00 +0000

    modified: ps_config/lib/__init__.py
    new class: ps_config

--- ps_config/lib/__init__.py
+++ ps_config/lib/__init__.py
@@ -1,54 +1,68 @@
 # coding: utf-8
 
 import ConfigParser
-
-config = ConfigParser.RawConfigParser()
-
-
-def get_sections(path):
-    config.read(path)
-    section_list = config.sections()
-
-    return section_list
+import os
+import sys
 
 
-def get_options(path, section):
-    config.read(path)
-    option_list = config.items(section)
+class ps_config():
 
-    return option_list
+    def __init__(self, path):
+        self.path = path
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(self.path)
 
+    def test_path(self):
+        if not os.path.exists(self.path):
+            print(self.path+" は存在しません。")
+            sys.exit(1)
 
-def get_option(path, section, option):
-    config.read(path)
-    option_value = config.get(section, option)
+    def get_section_list(self):
+        self.test_path()
+        section_list = self.config.sections()
+        return section_list
 
-    return option_value
+    def get_option_list(self, section_name):
+        self.test_path()
 
+        if not self.config.has_section(section_name):
+            print(section_name+" は存在しません。")
+            sys.exit(1)
 
-def remove_section(path, section):
-    config.read(path)
-    config.remove_section(section)
+        option_list = self.config.items(section_name)
+        return option_list
 
-    with open(path, 'wt') as file:
-        config.write(file)
+    def get_option_value(self, section_name, option_name):
+        self.test_path()
 
+        if not self.config.has_section(section_name):
+            print(section_name+" は存在しません。")
+            sys.exit(1)
 
-def remove_option(path, section, option):
-    config.read(path)
-    config.remove_option(section, option)
+        if not self.config.has_option(section_name,option_name):
+            print(option_name+" は存在しません。")
+            sys.exit(1)
 
-    with open(path, 'wt') as file:
-        config.write(file)
+        option_value = self.config.get(section_name, option_name)
+        return option_value
 
+    def set_content(self):
+        with open(self.path, "wt") as file:
+            self.config.write(file)
 
-def set_option(path, section, option, value):
-    config.read(path)
+    def remove_section(self, section_name):
+        self.test_path()
+        self.config.remove_section(section_name)
+        self.set_content()
 
-    if not config.has_section(section):
-        config.add_section(section)
+    def remove_option(self, section_name, option_name):
+        self.test_path()
+        self.config.remove_option(section_name, option_name)
+        self.set_content()
 
-    config.set(section, option, value)
+    def set_option(self, section_name, option_name, value):
+        if not self.config.has_section(section_name):
+            self.config.add_section(section_name)
 
-    with open(path, 'wt') as file:
-        config.write(file)
+        self.config.set(section_name, option_name, value)
+        self.set_content()
